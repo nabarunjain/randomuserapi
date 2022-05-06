267d3b4f11ad9c9dc5cb6be878317db0fd6760d7
Nabarun Jain <nabarun.jain@anu.edu.in>
2022-05-06 23:03:30 +0530

    Displaying the data of users using cards for the number of users entered by the new user

--- script.js
+++ script.js
@@ -20,20 +20,49 @@ window.onload = function() {
 
 const SearchButton = document.querySelector('.searchButton');
 const SearchBar = document.querySelector('.searchBar');
-const cardTemplate = document.querySelector('[data-card-template]')
+const cardTemplate = document.querySelector('[data-card-template]');
+const cardContainer = document.querySelector('[data-card-container]');
+
+let users = [];
+
+document.querySelector('.shBtn').addEventListener('click', () => {
+    console.log(users);
+})
 
 SearchButton.addEventListener('click', () => {
+    cardContainer.innerHTML = '';
     $.ajax({
-        url: 'https://randomuser.me/api/?inc=name,picture&results='+SearchBar.value,
+        url: 'https://randomuser.me/api/?inc=name,picture,email,location&results='+SearchBar.value,
         dataType: 'json',
         success: function(data) {
             console.log(data);
             const dataRes = data.results;
-            for( i=0; i<SearchBar.value; i++)
+            /*for( i=0; i<SearchBar.value; i++)
             {
                 const card = cardTemplate.content.cloneNode(true).children[0]; 
-                console.log(dataRes[i]);
-            }
+                const cardName = card.querySelector('[data-name]');
+                const cardMail = card.querySelector('[data-mail]');
+                const cardLocation = card.querySelector('[data-location]');
+                const cardImg = card.querySelector('[data-img]')
+                cardName.textContent = dataRes[i].name.title +" " +dataRes[i].name.first+" "+dataRes[i].name.last;
+                cardMail.textContent = dataRes[i].email;
+                cardLocation.textContent = "I am from "+dataRes[i].location.state;
+                cardImg.src = dataRes[i].picture.medium;
+                cardContainer.append(card);
+            }*/
+            users = dataRes.map(user => {
+                const card = cardTemplate.content.cloneNode(true).children[0]; 
+                const cardName = card.querySelector('[data-name]');
+                const cardMail = card.querySelector('[data-mail]');
+                const cardLocation = card.querySelector('[data-location]');
+                const cardImg = card.querySelector('[data-img]')
+                cardName.textContent = user.name.title +" " +user.name.first+" "+user.name.last;
+                cardMail.textContent = user.email;
+                cardLocation.textContent = "I am from "+user.location.state;
+                cardImg.src = user.picture.medium;
+                cardContainer.append(card);
+                return {first:user.name.first, title:user.name.title, last:user.name.last, email:user.email, picture:user.picture.medium, element : card};
+            });
         }
     })
 })
